6f2868b19fb8014f688b2c2dc63a341e5d4db016
Arun Ganesh <arun.planemad@gmail.com>
2016-11-24 15:01:14 +0530

    Add boilerplate map #1

---
+++ index.js
@@ -0,0 +1,33 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiY2l2dzVxbzA3MDAwNDJzbDUzMzVzbXc5dSJ9.WZ4_UtVvuVmOw4ofNMkiJw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    zoom: 1.4,
+    center: [21.6,7.6],
+    hash: true
+});
+
+map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+}));
+
+map.on('load', function () {
+    map.addSource('terrain-data', {
+        type: 'vector',
+        url: 'mapbox://mapbox.mapbox-terrain-v2'
+    });
+    map.addLayer({
+        "id": "terrain-data",
+        "type": "line",
+        "source": "terrain-data",
+        "source-layer": "contour",
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#ff69b4",
+            "line-width": 1
+        }
+    });
+});
