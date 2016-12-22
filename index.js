a32ab513410a39ffa3e809af4232cc2e14f6bde0
amishas157 <amishas157@gmail.com>
2016-12-22 18:58:31 +0530

    Add slider

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 /* global App */
 var mapboxglLive = require('./mapbox-gl-live');
 
+var threshold = 100;
 mapboxgl.accessToken = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiY2l2dzVxbzA3MDAwNDJzbDUzMzVzbXc5dSJ9.WZ4_UtVvuVmOw4ofNMkiJw';
 var map = new mapboxgl.Map({
     container: 'map',
@@ -31,33 +32,72 @@ map.on('load', function() {
                 "property": 'distance',
                 'stops': [
                     [
-                        0, 1
+                        0, 0
                     ],
                     [
-                        5, 4
+                        threshold/20, 4
                     ],
                     [
-                        10, 8
+                        threshold/10, 8
                     ],
-                    [100, 20]
+                    [threshold, 20]
                 ]
             },
             "circle-color": {
                 "property": 'distance',
                 "stops": [
                     [
-                        0, '#00ff00'
+                        0, '#ffffff'
                     ],
                     [
-                        5, '#ffff00'
+                        threshold/20, '#00ff00'
                     ],
-                    [10, '#ff0000']
+                    [threshold/10, '#ffff00'],
+                    [threshold, '#ff0000']
                 ]
             }
         }
     });
-
+    document.getElementById('slider').addEventListener('input', function(e) {
+        var distance = parseInt(e.target.value, 10);
+        threshold = distance;
+        repaintLayer(distance);
+    });
+    repaintLayer(100);
     // Inspect wikidata layer on click and show popup information
     mapboxglLive.inspector(map, {layers: ['wikidata-layer']});
 
 });
+
+function repaintLayer(threshold) {
+    map.setPaintProperty('wikidata-layer',"circle-radius", {
+        "property": 'distance',
+        'stops': [
+            [
+                0, 0
+            ],
+            [
+                threshold/20, 4
+            ],
+            [
+                threshold/10, 8
+            ],
+            [threshold, 20]
+        ]
+    });
+    map.setPaintProperty('wikidata-layer',"circle-color", {
+        "property": 'distance',
+        "stops": [
+            [
+                0, '#ffffff'
+            ],
+            [
+                threshold/20, '#00ff00'
+            ],
+            [threshold/10, '#ffff00'],
+            [threshold, '#ff0000']
+        ]
+    });
+    document.getElementById('Distance').textContent = threshold;
+
+}
